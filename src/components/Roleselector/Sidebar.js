a7dca4fab38aadba98fd1a599ef455577aa28a28
Dheeraj <i.dheerajdileep@gmail.com>
2023-05-10 17:16:51 +0530

    fixed problem with sidebar.js

--- src/components/Roleselector/Sidebar.js
+++ src/components/Roleselector/Sidebar.js
@@ -4,24 +4,23 @@ import logo from '../../Assets/Roleselector/whitelogo.png'
 import Typed from 'react-typed'
 function Sidebar() {
   return (
-    <div className='relative'>
-        <div className="bg-[#1D263A] w-1/3 h-screen flex flex-col z-[-1] overflow-hidden">
-  <img src={bgImg} className="w-full h-screen" alt="Background Image" />
-  <div className="absolute top-[3.75rem] left-[2.5rem]">
-    <img src={logo} alt="Logo" />
-    <div className='mt-[100px]'>
-    <Typed
-      className='md:text-2xl sm:text-2xl text-xl font-bold pl-2 text-white break-words '
-      strings={['“Behind every successful person,<br/>there is a generous sponsor who<br/> believed in their potential and <br/>invested in their future “ ']}
-      typeSpeed={120}
-      loop
-      backSpeed={140}
-    />
+    <div >
+        <div className="bg-[#1D263A] w-screen/3 h-screen flex flex-col z-[-1] overflow-hidden">
+            <img src={bgImg} className="w-full h-screen" alt="Background Image" />
+            <div className="absolute top-[3.75rem] left-[2.5rem]">
+                <img src={logo} alt="Logo" />
+                <div className='mt-[100px]'>
+                    <Typed
+                    className='md:text-2xl sm:text-2xl text-xl font-bold pl-2 text-white break-words '
+                    strings={['“Behind every successful person,<br/>there is a generous sponsor who<br/> believed in their potential and <br/>invested in their future “ ']}
+                    typeSpeed={120}
+                    loop
+                    backSpeed={140}
+                    />
+                </div>
+            </div>
+        </div>
     </div>
-  </div>
-</div>
-
-</div>
   )
 }
 
