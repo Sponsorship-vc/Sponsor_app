4cca81bb71369da3d0d2e5fbd869b3e71bf7a348
dhanush <dhanushpk50@gmail.com>
2023-05-10 15:58:17 +0530

    hero responsive

--- src/components/Landing/Hero.js
+++ src/components/Landing/Hero.js
@@ -4,25 +4,25 @@ import Brands from '../../Assets/Landing/Brands.png'
 function Hero() {
   return (
     <div>
-        <div className='flex md:flex-row mt-[110px] ml-[8%] flex-col h-full w-full'>
-            <div className='mt-[-25px]'>
-                <h1 className='font-bold text-dark-blue text-6xl '>Transform<br/>Your ideas<br/>into reality</h1>
+        <div className='flex md:flex-row flex-col h-full w-full'>
+            <div className=' mx-auto md:mx-0 pt-[85px] md:pl-[12%]'>
+                <h1 className='font-bold text-dark-blue text-4xl sm:text-6xl '>Transform<br/>Your ideas<br/>into reality</h1>
                 <p className='font-semibold mt-5 text-gray-600'>Connecting Sponsors and Creative Thinkers to Drive<br/> Impactful Projects</p>
                 <div>
-            <div className="bg-white shadow-2xl rounded-lg p-4 w-[680px] h-[75px]  flex-row hidden md:flex">
+            <div className="bg-white shadow-2xl rounded-lg p-4 w-[680px] h-[75px] flex justify-between flex-row hidden md:flex">
                     <div className="flex flex-col ">
                         <h2 className="text-lg font-semibold text-gray-800">Innovation categories</h2>
                         <p className='text-gray-600 text-sm'>What kind of innovative ideas do you want to sponsor ?</p>
                     </div>
-                    <button className="bg-blue-950 text-white font-bold py-2 px-8 rounded ml-[9.5rem] text-sm">
+                    <button className="bg-blue-950 text-white font-bold py-2 px-8 rounded text-sm">
                         Explore now
                     </button>
                 </div>
                 <p className='font-semibold mt-4'>Popular Search:  Electric Vehicles , Green Energy, Education </p>
                 </div>
             </div>
-            <div className='!m-[-75px] !ml-[-355px] z-[-1] '>
-                <img src={Heroimg} alt='heroimg'  className="object-contain w-[750px] h-[750px] ml-[200px] mt-[-70px]"/>
+            <div className='md:ml-[-250px] md:mt-[0px] mt-[-140px] z-[-1] '>
+                <img src={Heroimg} alt='heroimg'  className="object-contain w-[750px] h-[750px]"/>
             </div>
         </div>
         <img src={Brands}/>
