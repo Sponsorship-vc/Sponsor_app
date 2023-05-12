24e1cd43356676c01d5c0c006ff516c87dc0b694
dhanush <dhanushpk50@gmail.com>
2023-05-12 17:48:46 +0530

    header created

--- src/components/Dashboard/Dbpages/Dashb.js
+++ src/components/Dashboard/Dbpages/Dashb.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import Card from './Sponsor/Card'
+import Header from './Header'
 
 const Dashb = () => {
   return (
      
     <div>
+        <Header/>
     <div class="grid grid-cols-5  flex-1 h-screen  bg-white   items-center justify-center  p- bg-white   shadow ">
         <div class="col-start-2 flex-row">
         <Card/>
---
+++ src/components/Dashboard/Dbpages/Header.js
@@ -0,0 +1,75 @@
+import React from 'react'
+import { FaBell } from 'react-icons/fa';
+import { FiUser } from 'react-icons/fi';
+import { useLocation } from 'react-router-dom';
+import {
+    getDocs,
+    collection,
+  } from "firebase/firestore";
+  import { db } from "../../../firebase/config";
+  import { useEffect, useState } from "react";
+  import { getAuth } from "firebase/auth"
+  import { app } from '../../../firebase/config';
+
+
+
+
+function Header() {
+
+  const location = useLocation();
+  const path = location.pathname
+  const message = path.split('/').pop();
+  const [userList, setuserList] = useState([]);
+  const usersCollectionRef = collection(db, "users");
+  const auth = getAuth(app);
+
+
+  const getuserList = async () => {
+    try {
+      const data = await getDocs(usersCollectionRef);
+      const filteredData = data.docs.map((doc) => ({
+        ...doc.data(),
+        id: doc.id,
+      })).filter((doc) => doc.userId === auth.currentUser.uid);;
+      setuserList(filteredData);
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  useEffect(() => {
+    getuserList();
+  }, []);
+
+  
+
+  return (
+
+    <div className='flex-1 flex flex-row px-8 justify-between ml-64 py-4'>
+    <div>
+      <p>{message}</p>
+    </div>
+    <div className='flex flex-row gap-x-8'>
+        <div class="h-12 w-12 rounded-full flex bg-gray-400">
+            <FaBell className='  m-auto' />
+        </div>
+
+        {userList.map((user) => (
+            <div>
+                {user.name}.
+                <p>{user.role}</p>
+            </div>
+            
+        ))}
+        <div class="h-12 w-12 rounded-full flex bg-gray-400">
+            <FiUser className='m-auto'/>
+        </div>
+    </div>
+
+
+    </div>
+
+  )
+}
+
+export default Header
